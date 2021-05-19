ec9dc62dc5ca7106bbb08c1b45ca4250dce1806e
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-05-19 13:40:09 +0100

    Update seurat_to_scv.R

--- R/seurat_to_scv.R
+++ R/seurat_to_scv.R
@@ -1,7 +1,7 @@
 #' Convert a Seurat object to a scviewer-formatted `h5` file
 #' 
 #' @param h5_file Path to `h5` file
-#' @param seurat Seurat object
+#' @param seurat Seurat object or file path from which an `rds` can be read
 #' @param recalculate_reductions Should 2D and 3D tSNE and UMAP reductions be calculated?
 #' @param ... Arguments passed to `add_all_projections`
 #' 
@@ -10,14 +10,18 @@
 #' @export
 #' 
 seurat_to_scv <- function(h5_file, seurat, recalculate_reductions=FALSE, ...) {
-  if(recalculate_reductions)
-    seurat %<>% add_all_projections(...)
-
   if(file.exists(h5_file)) {
     sprintf(fmt='- the h5_file (%s) exists and will not be removed!', h5_file) %>% message()
   } else {
     h5createFile(file=h5_file) %>% invisible()
   }
+
+  seurat %<>%
+    when(is.character(.)~readRDS(.),
+         is_in(x='Seurat', table=class(.))~.,
+         TRUE~stop('!!! seurat is not a character or Seurat')) %>%
+    when(recalculate_reductions~add_all_projections(seurat=., ...),
+         TRUE~.)
 
   write_reductions(h5_file=h5_file, seurat=seurat)
   write_features(h5_file=h5_file, seurat=seurat, ...)
